8082f8bd5078dd5157bd2f785a7df610f4869853
lenyshaone <71610989+lenyshaone@users.noreply.github.com>
2020-11-03 16:24:51 +0300

    Update apitest.java

--- src/test/java/apitest.java
+++ src/test/java/apitest.java
@@ -2,7 +2,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 public class apitest {
